4d9e2dcc49d74daf8e2001160ddfdba5ea0023fb
QuentinRK <q.rankin@outlook.com>
2022-04-24 15:21:40 +0100

    added nginx and frontend public files

---
+++ backend/wsgi-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+gunicorn backend.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
+
+exec "$@"
